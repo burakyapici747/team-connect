31f2ad53319b0a3c4881f4ed1a0a575e35f0a7a9
burakyapici <burakyapici747@gmail.com>
2024-12-18 23:16:28 +0300

    fix: handle missing user errors with custom exception

---
+++ src/main/java/com/teamconnect/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.teamconnect.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
