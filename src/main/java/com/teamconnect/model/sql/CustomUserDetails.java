48aae21c8cdf7fada8945a79891c4ffa54da52c2
burakyapici <burakyapici747@gmail.com>
2024-12-22 15:27:20 +0300

    refactor: Improve CustomUserDetails implementation by adding getId method, returning authorities, email, and password correctly

--- src/main/java/com/teamconnect/model/sql/CustomUserDetails.java
+++ src/main/java/com/teamconnect/model/sql/CustomUserDetails.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 
 public class CustomUserDetails implements UserDetails {
     private final String id;
@@ -16,26 +15,31 @@ public class CustomUserDetails implements UserDetails {
             String id,
             String email,
             String password,
-            Collection<? extends GrantedAuthority> authorities) {
+            Collection<? extends GrantedAuthority> authorities
+    ) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.authorities = authorities;
     }
 
+    public String getId() {
+        return id;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return email;
     }
 
     @Override
