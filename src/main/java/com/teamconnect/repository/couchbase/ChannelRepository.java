eeea63f7e695301c1328cbcebca50f98c3a7deee
burakyapici <burakyapici747@gmail.com>
2025-01-17 23:43:32 +0300

    refactor: update UserRole model and enhance repository interfaces
    - Simplified UserRole entity by removing unnecessary relationships and annotations, focusing on role management.
    - Updated WebSocketMessage to use authorId and channelId for clarity in message handling.
    - Enhanced ChannelRepository and MessageRepository with new query methods for retrieving channels and messages by user and channel IDs, respectively.
    - Introduced ChannelService interface method for fetching channels by user ID, improving service functionality.

--- src/main/java/com/teamconnect/repository/couchbase/ChannelRepository.java
+++ src/main/java/com/teamconnect/repository/couchbase/ChannelRepository.java
@@ -1,4 +1,15 @@
 package com.teamconnect.repository.couchbase;
 
-public interface ChannelRepository {
+import com.teamconnect.model.nosql.Channel;
+import org.springframework.data.couchbase.repository.CouchbaseRepository;
+import org.springframework.data.couchbase.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ChannelRepository extends CouchbaseRepository<Channel, String> {
+    @Query("#{#n1ql.selectEntity} WHERE #{#n1ql.filter} " +
+           "AND ARRAY_CONTAINS(recipients, $userId) " +
+           "AND type = 'DIRECT_CHANNEL'")
+    List<Channel> findChannelsByUserId(@Param("userId") String userId);
 }
