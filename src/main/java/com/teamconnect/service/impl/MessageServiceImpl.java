1d71a4964bf707260cc79943c31f06da7c9e2294
burakyapici <burakyapici747@gmail.com>
2025-01-23 23:52:07 +0300

    feat: implement message sending functionality and improve WebSocket event handling
    - Added a new method in MessageServiceImpl for sending messages, which includes creating a message entity, saving it to the repository, and sending it to a RabbitMQ exchange.
    - Updated the constructor of MessageServiceImpl to include RabbitTemplate for message publishing.
    - Refactored WebSocketEventListener to remove the WebSocketMessageService dependency and log user connection and disconnection events instead.
    - Modified TeamServiceImpl to create a DM channel queue with a hardcoded value for testing purposes.
    
    These changes enhance the messaging capabilities of the application and improve the handling of WebSocket events.

--- src/main/java/com/teamconnect/service/impl/MessageServiceImpl.java
+++ src/main/java/com/teamconnect/service/impl/MessageServiceImpl.java
@@ -1,5 +1,6 @@
 package com.teamconnect.service.impl;
 
+import com.teamconnect.api.input.message.MessageCreateInput;
 import com.teamconnect.api.output.user.AuthorOutput;
 import com.teamconnect.dto.MessageDto;
 import com.teamconnect.model.nosql.Message;
@@ -7,11 +8,14 @@ import com.teamconnect.model.sql.User;
 import com.teamconnect.repository.couchbase.MessageRepository;
 import com.teamconnect.repository.postgresql.UserRepository;
 import com.teamconnect.service.MessageService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -19,10 +23,16 @@ import java.util.stream.Collectors;
 public class MessageServiceImpl implements MessageService {
     private final UserRepository userRepository;
     private final MessageRepository messageRepository;
+    private final RabbitTemplate rabbitTemplate;
 
-    public MessageServiceImpl(MessageRepository messageRepository, UserRepository userRepository) {
+    public MessageServiceImpl(
+        MessageRepository messageRepository,
+        UserRepository userRepository,
+        RabbitTemplate rabbitTemplate
+    ) {
         this.userRepository = userRepository;
         this.messageRepository = messageRepository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @Override
@@ -69,5 +79,46 @@ public class MessageServiceImpl implements MessageService {
                 );
             })
             .collect(Collectors.toList());
+    }
+
+    @Override
+    public MessageDto sendMessage(String channelId, String authorId,  MessageCreateInput messageCreateInput) {
+        Message message = new Message();
+        message.setId(UUID.randomUUID().toString());
+        message.setAuthorId(authorId);
+        message.setChannelId(channelId);
+        message.setContent(messageCreateInput.content());
+        message.setTimestamp(Instant.now());
+        message.setEditedTimestamp(null);
+        message.setPinned(false);
+        message.setType(1);
+        message.setAttachments(null);
+        message.setMentions(null);
+        message.setReactions(null);
+
+        messageRepository.save(message);
+
+        rabbitTemplate.convertAndSend("dm.exchange.dccd525d-d909-4cec-99f4-28cb459eb9a5", channelId, message.getId());
+
+        User author = userRepository.findById(authorId).orElseThrow();
+
+        return new MessageDto(
+            message.getId(),
+            message.getChannelId(),
+            message.getContent(),
+            message.getTimestamp(),
+            message.getEditedTimestamp(),
+            message.getPinned(),
+            message.getType(),
+            message.getAttachments(),
+            message.getMentions(),
+            message.getReactions(),
+            new AuthorOutput(
+                author.getId(),
+                author.getUsername(),
+                author.getUserProfile().getAvatarFileUrl(),
+                author.getUserProfile().getAvatarFileId()
+            )
+        );
     }
 }
