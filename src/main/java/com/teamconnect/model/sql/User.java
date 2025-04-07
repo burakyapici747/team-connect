7ab89f98c7349e2b0de23dddc81027f4031c8154
burakyapici <burakyapici747@gmail.com>
2025-04-07 22:54:20 +0300

    refactor(model): use Lombok @Getter and @Setter in User and Friendship models

--- src/main/java/com/teamconnect/model/sql/User.java
+++ src/main/java/com/teamconnect/model/sql/User.java
@@ -2,12 +2,17 @@ package com.teamconnect.model.sql;
 
 import com.teamconnect.common.enumarator.UserStatus;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "USERS")
+@Getter
+@Setter
 public class User extends BaseModel {
     @Column(name = "username", unique = true)
     private String username;
@@ -46,100 +51,4 @@ public class User extends BaseModel {
 
     @OneToMany(mappedBy = "user2", fetch = FetchType.LAZY)
     private Set<Friendship> receivedFriendships = new HashSet<>();
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAvatarUrl() {
-        return avatarUrl;
-    }
-
-    public void setAvatarUrl(String avatarUrl) {
-        this.avatarUrl = avatarUrl;
-    }
-
-    public UserStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(UserStatus status) {
-        this.status = status;
-    }
-
-    public Instant getLastSeenAt() {
-        return lastSeenAt;
-    }
-
-    public void setLastSeenAt(Instant lastSeenAt) {
-        this.lastSeenAt = lastSeenAt;
-    }
-
-    public UserProfile getUserProfile() {
-        return userProfile;
-    }
-
-    public void setUserProfile(UserProfile userProfile) {
-        this.userProfile = userProfile;
-    }
-
-    public Set<Team> getOwnedTeams() {
-        return ownedTeams;
-    }
-
-    public void setOwnedTeams(Set<Team> ownedTeams) {
-        this.ownedTeams = ownedTeams;
-    }
-
-    public Set<TeamMember> getTeamMembers() {
-        return teamMembers;
-    }
-
-    public void setTeamMembers(Set<TeamMember> teamMembers) {
-        this.teamMembers = teamMembers;
-    }
-
-    public Set<UserRole> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<UserRole> roles) {
-        this.roles = roles;
-    }
-
-    public Set<Friendship> getSentFriendships() {
-        return sentFriendships;
-    }
-
-    public void setSentFriendships(Set<Friendship> sentFriendships) {
-        this.sentFriendships = sentFriendships;
-    }
-
-    public Set<Friendship> getReceivedFriendships() {
-        return receivedFriendships;
-    }
-
-    public void setReceivedFriendships(Set<Friendship> receivedFriendships) {
-        this.receivedFriendships = receivedFriendships;
-    }
 }
